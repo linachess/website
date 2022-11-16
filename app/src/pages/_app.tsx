55a0c5b2a85115afb8a76e64a4b91f6ee48e9138
Bartholomé Gili <dev.bartho@gmail.com>
2022-11-16 20:37:22 +0000

    feat(app/pages): fade in transition between pages

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@fontsource/open-sans/700.css'
 import '@fontsource/poppins/900.css'
 import '@fontsource/raleway/400.css'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ScaleFade } from '@chakra-ui/react'
 import { theme } from '@core/theme'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { trpc } from '@utils/lib'
@@ -11,7 +11,7 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
 	
   	return (
 		<ChakraProvider theme={theme}>
@@ -20,7 +20,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 					'client-id': process.env['NEXT_PUBLIC_PAYPAL_CLIENT_ID'],
 					currency: 'EUR'
 				}}>
-					<Component {...pageProps} />
+					<ScaleFade
+						key={router.route}
+						initialScale={0.9}
+						in={true}
+					>
+						<Component {...pageProps} />
+					</ScaleFade>
 				</PayPalScriptProvider>
 			</QueryClientProvider>
 		</ChakraProvider>
